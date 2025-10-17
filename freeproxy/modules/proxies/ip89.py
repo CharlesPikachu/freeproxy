160bb08f8919027ec2abd022aab6b1276fd4d307
CharlesPikachu <1159254961@qq.com>
2025-10-17 21:16:11 +0800

    fix zdaye

--- freeproxy/modules/proxies/ip89.py
+++ freeproxy/modules/proxies/ip89.py
@@ -32,12 +32,12 @@ class IP89ProxiedSession(BaseProxiedSession):
             'Cookie': 'SITE_TOTAL_ID=28e0ae30b9d2b0a987fabb84a43c5463; http_waf_cookie=38d5a796-a68d-4bb3f36d6f0f123e32f1b6a198ef83571b51; http_ydclearance=8ffb1bf90adcc70a451284e5-bba4-47be-8b1d-71a706be914b-1760705955'
         }
         url = 'http://api.89ip.cn/tqdl.html?api=1&num=1000&port=&address=&isp='
-        resp = requests.get(url, headers=headers, verify=False)
+        resp = requests.get(url, headers=headers)
         if resp.status_code != 200: return self.candidate_proxies
         for item in resp.text.split('<br>'):
             if PATTERN.fullmatch(item.strip()) is None: continue
             self.candidate_proxies.append({
-                'http': f"http://{item}", 'https': f"https://{item}"
+                'http': f"http://{item}", 'https': f"http://{item}",
             })
         # return
         return self.candidate_proxies
